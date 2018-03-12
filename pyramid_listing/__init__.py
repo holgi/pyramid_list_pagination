45fde0a43be9de0e22ee25f1ec07be6cf60f984f
Holger Frey <mail@holgerfrey.de>
2018-03-12 14:16:40 +0100

    exposing classes and functions in __init__.py

--- pyramid_listing/__init__.py
+++ pyramid_listing/__init__.py
@@ -3,3 +3,8 @@
 __author__ = 'Holger Frey'
 __email__ = 'mail@holgerfrey.de'
 __version__ = '0.1.2'
+
+
+from .listing import SQLAlchemyListing
+from .pagination import Pagination, includeme
+from .resource import ListingResource
